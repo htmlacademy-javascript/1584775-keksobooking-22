a7dd8a422e2f0e48ed59ccb718fde034bef306eb
Anastasia <70826807+Juliet94@users.noreply.github.com>
2021-01-31 18:16:13 +0300

    Добавлена функция

--- js/main.js
+++ js/main.js
@@ -1,21 +1,21 @@
+const getRandomNumber = function (min, max) {
+  return Math.random() * (max - min + 1) + min;
+}
+
 const getRandomInteger = function (min, max) {
   if (min < 0 || min >= max) {
-    // eslint-disable-next-line no-console
-    console.error('Значение min должно быть больше 0 и меньше max!');
-    return;
+    throw new Error('Значение min должно быть больше 0 и меньше max!');
   }
 
-  return Math.floor(Math.random() * (max - min + 1) ) + min;
+  return Math.floor(getRandomNumber(min, max));
 }
 
 const getRandomFloat = function (min, max, digit) {
   if (min < 0 || min >= max) {
-    // eslint-disable-next-line no-console
-    console.error('Значение min должно быть больше 0 и меньше max!');
-    return;
+    throw new Error('Значение min должно быть больше 0 и меньше max!');
   }
 
-  return (Math.random() * (max-min) + min).toFixed(digit);
+  return getRandomNumber(min, max).toFixed(digit);
 }
 
 getRandomInteger(5,10);
